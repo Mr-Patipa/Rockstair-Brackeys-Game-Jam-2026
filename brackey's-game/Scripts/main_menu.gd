1cd63a38ce8f4a224110257f0f5a543e2be3d9ef
Fazle <thedistortedvoid@gmail.com>
2026-08-26 11:36:32 +0700

    Game Scene Distribution & Main Menu Base UI Layout

---
+++ brackey's-game/Scripts/main_menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@export var start: Button
+@export var settings: Button
+@export var exit: Button
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	start.pressed.connect(_on_start_button_pressed)
+	settings.pressed.connect(_on_settings_button_pressed)
+	exit.pressed.connect(_on_exit_button_pressed)
+	
+
+func _on_start_button_pressed() -> void:
+	pass 
+	
+func _on_settings_button_pressed() -> void:
+	pass
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()

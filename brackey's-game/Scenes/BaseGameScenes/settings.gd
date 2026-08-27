fb56fe71e93112c0c2e0b43dfeaba4c20533a1f1
Fazle <thedistortedvoid@gmail.com>
2026-08-27 13:56:09 +0700

    Functional Volume Slider
    Other than the fact we have no sound yet...

--- brackey's-game/Scenes/BaseGameScenes/settings.gd
+++ brackey's-game/Scenes/BaseGameScenes/settings.gd
@@ -1,18 +1,42 @@
 extends Control
 
-#VOLUME
-@export var master_h_slider: HSlider
+@export_group("Volume")
+@export_subgroup("Slider Node")
+@export var master_h_slider = HSlider
 @export var music_h_slider: HSlider
 @export var sfx_h_slider: HSlider
 
-#DISPLAY
+@export_subgroup("Audio Bus")
+@export var master_audio_bus = "Master"
+@export var music_audio_bus = "Music"
+@export var sfx_audio_bus = "SFX"
+
+@export_group("Display")
 @export var option_button: OptionButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	master_h_slider.value_changed.connect(_on_master_volume_changed)
+	music_h_slider.value_changed.connect(_on_music_volume_changed)
+	sfx_h_slider.value_changed.connect(_on_sfx_volume_changed)
+	
+	option_button.item_selected.connect(_on_screen_resolution_changed)
 
+func _on_master_volume_changed(value) -> void:
+	print(value)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_music_volume_changed(value) -> void:
+	print(value)
+	
+func _on_sfx_volume_changed(value) -> void:
+	print(value)
+	
+func _on_screen_resolution_changed(value) -> void:
+	print(value)
+	
+	if value == 0:
+		pass
+	elif value == 1:
+		pass
+	elif value == 2:
+		pass

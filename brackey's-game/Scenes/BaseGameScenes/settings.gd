118cba1fd1228b25a15013b44781b7b594ab901c
Fazle <thedistortedvoid@gmail.com>
2026-08-27 13:06:39 +0700

    Base Settings UI

---
+++ brackey's-game/Scenes/BaseGameScenes/settings.gd
@@ -0,0 +1,18 @@
+extends Control
+
+#VOLUME
+@export var master_h_slider: HSlider
+@export var music_h_slider: HSlider
+@export var sfx_h_slider: HSlider
+
+#DISPLAY
+@export var option_button: OptionButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
